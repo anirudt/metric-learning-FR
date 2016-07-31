227b339f4f7b2c0147cb25a3af0c8566be9d6b96
anirudt <anirudt@gmail.com>
2016-07-31 11:46:44 +0530

    added gitignore, and working eigenfaces

--- vanilla-fr/eigenfaces.py
+++ vanilla-fr/eigenfaces.py
@@ -3,6 +3,7 @@ import numpy as np
 from time import time
 from sklearn.decomposition import PCA
 import cv2
+import pdb
 
 """ Script for EigenFace Method of Face Recognition """
 
@@ -12,40 +13,64 @@ dims         = (100, 100)
 
 def train():
     """ Get data, train, get the Eigenvalues and store them."""
-    face_matrix = np.array([ np.resize(np.array(cv2.imread("data/ROLL ("+str(num)+")/Regular/W (2).jpg", cv2.IMREAD_GRAYSCALE)), dims).ravel() for num in range(1, NUM_IMGS+1) ])
+    face_matrix = np.array([ np.resize(np.array(cv2.imread("data/ROLL ("+str(num)+")/Regular/W (2).jpg", cv2.IMREAD_GRAYSCALE), dtype='float64'), dims).ravel() for num in range(1, NUM_IMGS+1) ], dtype='float64')
     print "The dimensions of the face matrix are: {0}".format(face_matrix.shape)
 
     mean = np.mean(face_matrix, axis=0)
     print mean.shape
     face_matrix -= mean
 
-    pca = PCA(n_components=8)
+    """
+    pca = PCA(n_components=10)
     pca.fit(face_matrix.T)
-
     print "Displaying the EigenValues selected: {0}".format(pca.explained_variance_ratio_)
     eigen_vals = pca.explained_variance_ratio_
     eigen_vecs = pca.components_
 
-    features = np.dot(eigen_vecs, face_matrix)
-    print features.shape
-    return features, mean
+    """
+    print face_matrix.shape
+    cov = np.matrix(face_matrix) * np.matrix(face_matrix.T)
+    cov /= NUM_IMGS
+    print cov.shape
+    eigen_vals, eigen_vecs = np.linalg.eig(cov)
+    eigen_vals = np.abs(eigen_vals)
+    sort_indices = eigen_vals.argsort()[::-1]
+    eigen_vals = eigen_vals[sort_indices]
+    eigen_vecs = eigen_vecs[sort_indices]
 
-def test(features, mean):
+    print eigen_vecs.shape, eigen_vals.shape
+    print eigen_vecs, eigen_vals
+
+    # TODO: Conduct slicing.
+
+    eigen_vecs = np.matrix(eigen_vecs) * np.matrix(face_matrix)
+    norms = np.linalg.norm(eigen_vecs, axis=0)
+    eigen_vecs /= norms
+
+    weights = np.matrix(face_matrix) * np.matrix(eigen_vecs.T)
+    print weights.shape
+    #pdb.set_trace()
+    return eigen_vecs, weights, mean
+
+def test(eigen_vecs, weights, mean):
     """ Acquire a new image and get the data. """
-    test_image = np.resize(np.array(cv2.imread("", cv2.IMREAD_GRAYSCALE)), dims).ravel()
+    test_image = np.resize(np.array(cv2.imread("data/ROLL (7)/Regular/W (2).jpg", cv2.IMREAD_GRAYSCALE), dtype='float64'), dims).ravel()
 
     test_image -= mean
 
-    test_face = np.dot(features, test_image)
-
     # TODO: Compute the features for all other people and then conduct a nearest neighbour.
-    similarity_feat = np.linalg.norm(features - test_face)
+    print eigen_vecs.shape, test_image.shape
+    proj_weights = np.dot(eigen_vecs, test_image)
 
+    
+    similarity_feat = np.linalg.norm(weights - proj_weights, axis=1)
+    print similarity_feat
     detected_idx = np.argmin(similarity_feat)
-    print "Detected face is of serial no. {0}".format(detected_idx)
+    print "Detected face is of serial no. {0}".format(detected_idx+1)
+    ##pdb.set_trace()
 
 
 if __name__ == "__main__":
-    features, mean = train()
-    test(features, mean)
+    eigen_vecs, weights, mean = train()
+    test(eigen_vecs, weights, mean)
     print "We are done."

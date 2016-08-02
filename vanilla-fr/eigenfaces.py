e56baee828c7915e8b43fbcf94d1bf7071ed73c7
anirudt <anirudt@gmail.com>
2016-08-02 06:26:57 +0530

    added multi_runner scritpt

--- vanilla-fr/eigenfaces.py
+++ vanilla-fr/eigenfaces.py
@@ -4,6 +4,7 @@ from time import time
 from sklearn.decomposition import PCA
 import cv2
 import pdb
+import csv
 
 """ Script for EigenFace Method of Face Recognition """
 
@@ -28,16 +29,22 @@ def display_imgs(face_matrix):
     """
 
 def eigen_logger(eigen_vals, eigen_vecs):
-    
     """
     TODO: Identify the class of photos and place the eigenvalues and eigenvectors in a fairly understandable
     data structure. Also, try to plot the eigenvalue projections across all data fitted hitherto.
     Things to save: del A, del (eig_vals A), del (eig_vecs A)
     """
+    with open("eigen_vals.csv", "wb") as f:
+        writer = csv.writer(f)
+        writer.writerows(eigen_vals)
+    with open("eigen_vecs.csv", "wb") as f:
+        writer = csv.writer(f)
+        writer.writerows(eigen_vecs)
 
-def train():
+
+def train(tilt_idx):
     """ Get data, train, get the Eigenvalues and store them."""
-    face_matrix = np.array([ np.resize(np.array(cv2.imread("data/ROLL ("+str(num)+")/Regular/W (2).jpg", cv2.IMREAD_GRAYSCALE), dtype='float64'), dims).ravel() for num in range(1, NUM_IMGS+1) ], dtype='float64')
+    face_matrix = np.array([ np.resize(np.array(cv2.imread("data/ROLL ("+str(num)+")/Regular/W ("+str(tilt_idx)+").jpg", cv2.IMREAD_GRAYSCALE), dtype='float64'), dims).ravel() for num in range(1, NUM_IMGS+1) ], dtype='float64')
     print "The dimensions of the face matrix are: {0}".format(face_matrix.shape)
 
     mean = np.mean(face_matrix, axis=0)
@@ -66,11 +73,11 @@ def train():
     weights = np.matrix(face_matrix) * np.matrix(eigen_vecs.T)
     print weights.shape
     #pdb.set_trace()
-    return eigen_vecs, weights, mean
+    return eigen_vals, eigen_vecs, weights, mean
 
-def test(eigen_vecs, weights, mean):
+def test(tilt_idx, eigen_vecs, weights, mean):
     """ Acquire a new image and get the data. """
-    test_image = np.resize(np.array(cv2.imread("data/ROLL (9)/Regular/W (2).jpg", cv2.IMREAD_GRAYSCALE), dtype='float64'), dims).ravel()
+    test_image = np.resize(np.array(cv2.imread("data/ROLL (9)/Regular/W ("+str(tilt_idx)+").jpg", cv2.IMREAD_GRAYSCALE), dtype='float64'), dims).ravel()
 
     test_image -= mean
 
@@ -85,8 +92,20 @@ def test(eigen_vecs, weights, mean):
     print "Detected face is of serial no. {0}".format(detected_idx+1)
     ##pdb.set_trace()
 
+def multi_runner():
+    """
+    Runs the training and test for all the different tilted faces. Returns a list of lists of eigenvalues and eigenvectors.
+    """
+    eigenvals, eigenvecs = [], []
+    for tilt in range(5):
+        tmp_eigen_vals, tmp_eigen_vecs, tmp_weights, tmp_mean = train(tilt_idx)
+        test(tilt_idx, tmp_eigen_vecs, tmp_weights, tmp_mean)
+        eigenvals.append(tmp_eigen_vals)
+        eigenvecs.append(tmp_eigen_vecs)
+    return eigen_vals, eigen_vecs
+
 
 if __name__ == "__main__":
-    eigen_vecs, weights, mean = train()
-    test(eigen_vecs, weights, mean)
+    eigen_vals, eigen_vecs = multi_runner()
+    eigen_logger(eigen_vals, eigen_vecs)
     print "We are done."

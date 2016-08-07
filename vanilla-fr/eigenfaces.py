a750a741fdb665961f64596ad0a04f4c8e06d406
anirudt <anirudt@gmail.com>
2016-08-07 13:06:04 +0530

    added lda work

--- vanilla-fr/eigenfaces.py
+++ vanilla-fr/eigenfaces.py
@@ -46,49 +46,58 @@ def eigen_logger(eigen_vals, eigen_vecs):
 
 def import_training_set():
     """ Get the face matrix here. """
-    face_matrix = np.array([ np.resize(np.array(cv2.imread("data/ROLL ("+str(num)+")/Regular/W ("+str(tilt_idx)+").jpg", cv2.IMREAD_GRAYSCALE), dtype='float64'), dims).ravel() for num in range(1, NUM_IMGS+1) ], dtype='float64')
+    face_matrix = np.array([ np.resize(np.array(cv2.imread("data/ROLL ("+str(num)+")/Regular/W ("+str(tilt_idx)+").jpg", cv2.IMREAD_GRAYSCALE), dtype='float64'), dims).ravel() for num in range(1, NUM_IMGS+1) for tilt_idx in range(2,3)], dtype='float64')
     face_matrix = face_matrix.T
     print "The dimensions of the face matrix are: {0}".format(face_matrix.shape)
 
     mean = np.mean(face_matrix, axis=1)
-    print "The dimensions of the mean face are: {0}".format(mean)
+    print "The dimensions of the mean face are: {0}".format(mean.shape)
 
     # TODO: Make a way to print / imwrite this average image
-    face_matrix -= mean
+    for col in range(face_matrix.shape[1]):
+        face_matrix[:, col] = face_matrix[:, col] - mean
 
     return face_matrix
 
 def lda(eigen_face):
     """ Computes the LDA in the specified subspace provided. """
-    class_means = np.zeros(eigen_face.shape[0], NUM_PEOPLE)
-    within_class_cov = np.zeros(eigen_face.shape[0], eigen_face.shape[0])
-    between_class_cov = np.zeros(eigen_face.shape[0], eigen_face.shape[0])
+    class_means = np.zeros((eigen_face.shape[0], NUM_PEOPLE))
+    within_class_cov = np.zeros((eigen_face.shape[0], eigen_face.shape[0]))
+    between_class_cov = np.zeros((eigen_face.shape[0], eigen_face.shape[0]))
     for i in range(NUM_PEOPLE):
-        class_means[:,i] = np.mean(eigen_face[:,i*IMGS_PER_PERSON:i*IMGS_PER_PERSON+IMGS_PER_PERSON], axis=1)
+        class_means[:,i] = np.mean(eigen_face[:,i*IMGS_PER_PERSON:i*IMGS_PER_PERSON+IMGS_PER_PERSON], axis=1).ravel()
 
-    overall_mean = np.mean(class_means, axis=1)
+    overall_mean = np.mean(class_means, axis=1).ravel()
     for i in range(NUM_PEOPLE):
         class_mean_i = class_means[:, i]
-        class_mat = eigen_face[:, i*IMGS_PER_PERSON:(i+1)*IMGS_PER_PERSON] - class_means
+        class_mat = np.zeros((eigen_face.shape[0], IMGS_PER_PERSON))
+        #class_mat = np.matrix(eigen_face[:, i*IMGS_PER_PERSON:(i+1)*IMGS_PER_PERSON]) - class_mean_i
+        for j in range(i*IMGS_PER_PERSON, (i+1)*IMGS_PER_PERSON):
+            class_mat[:, j-i*IMGS_PER_PERSON] = eigen_face[:, j].ravel() - class_mean_i
         within_class_cov += np.matrix(class_mat) * np.matrix(class_mat.T)
 
-        between_class_cov += np.matrix(class_mean_i - overall_mean) * np.matrix((class_mean_i - overall_mean).T)
+        diff_mat = (class_mean_i - overall_mean).reshape((eigen_face.shape[0], 1))
+        print diff_mat.shape
+        between_class_cov += np.matrix(diff_mat) * np.matrix(diff_mat.T)
 
     print "Dimensions of within class scatter matrix are {0}".format(within_class_cov.shape)
     print "Dimensions of between class scatter matrix are {0}".format(between_class_cov.shape)
 
     eigen_vals, eigen_vecs = np.linalg.eig(np.matrix(np.linalg.inv(within_class_cov)) * np.matrix(between_class_cov))
+    # TODO: Select only some components based on some selection theory
 
-    # TODO: Determine the dimensions of the eigenvector and choose the appropriate projection.
+    lda_projection = np.matrix(eigen_vecs.T) * np.matrix(eigen_face)
 
+    print "The dimensions of the LDA projection are {0}".format(lda_projection.shape)
 
+    return lda_projection
 
 def pca(X, A):
     """ Computes the PCA of:
         X: covariance matrix
         A: difference matrix
     """
-    eigen_vals, eigen_vecs = np.linalg.eig(cov)
+    eigen_vals, eigen_vecs = np.linalg.eig(X)
     eigen_vals = np.abs(eigen_vals)
     sort_indices = eigen_vals.argsort()[::-1]
     eigen_vals = eigen_vals[sort_indices[0:10]]
@@ -103,8 +112,6 @@ def pca(X, A):
     eigen_face_space = np.matrix(selected_eigen_vecs.T) * np.matrix(A)
     return eigen_face_space
 
-
-
 def train(tilt_idx):
     """ Get data, train, get the Eigenvalues and store them."""
     face_matrix = import_training_set()
@@ -114,7 +121,10 @@ def train(tilt_idx):
     cov /= NUM_IMGS
     print cov.shape
 
-    eigen_face_space = pca(face_matrix, cov)
+    eigen_face_space = pca(cov, face_matrix)
+
+    # TODO: Return something
+    lda_projection = lda(eigen_face_space)
 
     eigen_vecs = np.matrix(eigen_vecs) * np.matrix(face_matrix)
     norms = np.linalg.norm(eigen_vecs, axis=0)

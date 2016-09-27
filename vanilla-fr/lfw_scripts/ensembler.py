d15fc4a36197ab32422cd05bf6ed2f885ca44ebb
anirudt <anirudt@gmail.com>
2016-09-28 05:27:07 +0530

    adding ensembler framework

--- vanilla-fr/lfw_scripts/ensembler.py
+++ vanilla-fr/lfw_scripts/ensembler.py
@@ -19,10 +19,23 @@ For the kNN-like probabilities, we use a modified Softmax
 for determining them.
 """
 
-def assemble():
+def generic_model_fitter(ml, X_train, y_train, X_test, y_test):
+    """ Takes a generic ML model and fits it with the data """
+    ml.fit(X_train, y_train)
+
+
+def assemble(X_train, y_train, X_test, y_test):
   """ Receives the training and test set samples and
-  performs metric learning algorithms followed by an
+  performs metric learning algorithms followed by a
   baseline ensemble classifier """
+
+  # Step 1: Spawn threads and generate class probabilities
+
+  # Step 2: Combine all of them and use a baseline ensemble classifier
+
+  # Get the classifier
+  
+
 
 if __name__ == '__main__':
   assemble()
